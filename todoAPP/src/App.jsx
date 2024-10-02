3fb02e8a5724edcd79ffeb363804d882760ea0d9
Abhi <abhikshirsagar1999@gmail.com>
2024-10-02 12:41:02 +0530

    RTK Todo is ready to deploy.

---
+++ todoAPP/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddTodos from "./components/AddTodos";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <AddTodos />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
